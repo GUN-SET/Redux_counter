c971d1dd8a7baa4a71cbf30dbe5cd6ef15c91c1e
X <androysol@gmail.com>
2021-11-17 21:52:07 +0200

    React added to the project

--- src/index.js
+++ src/index.js
@@ -1,39 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {bindActionCreators, createStore} from "redux";
+import {createStore} from "redux";
 import reduser from "./reducer";
-import * as actions from './actions';
+import App from "./components/App";
+import {Provider} from "react-redux";
 
 const store = createStore(reduser)
 
-const {dispatch, subscribe, getState} = store;
-
-const update = () => {
-    document.getElementById('counter').textContent = getState().value;
-}
-
-subscribe(update);
-
-// const bindActionCreator = (creator, dispatch) => (...args) => {
-//     dispatch(creator(...args))
-// }
-
-const {inc, dec, rnd} = bindActionCreators(actions, dispatch);
-
-document.getElementById('inc').addEventListener('click', inc)
-
-document.getElementById('dec').addEventListener('click', dec)
-
-document.getElementById('rnd').addEventListener('click', ()=> {
-    const value = Math.floor(Math.random() * 10);
-    rnd(value)
-})
 
 ReactDOM.render(
     <React.StrictMode>
-        <>
-
-        </>
+        <Provider store={store}>
+            <App/>
+        </Provider>
     </React.StrictMode>,
     document.getElementById('root')
 );
+
+
+
